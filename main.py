344c3a51d0498d075df381908f96ba8e22ea0d17
JoseEnriqueMendo <dinofiesta123@gmail.com>
2023-05-17 10:01:28 -0500

    ventanas actualizadas

--- main.py
+++ main.py
@@ -13,4 +13,14 @@ def information():
     return render_template('information.html')
 
 
+@app.route("/pentacam")
+def pentacam():
+    return render_template('pentacam.html')
+
+
+@app.route("/casoClinico")
+def casoClinico():
+    return render_template('casoClinico.html')
+
+
 app.run(debug=True)
